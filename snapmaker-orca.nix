b97f71e7d16e398d2b229b278ffd078c12e41b4e
Aaron Davis <aaron.m.davis@comcast.net>
2026-03-17 17:19:14 -0500

    comment update screen patch

--- snapmaker-orca.nix
+++ snapmaker-orca.nix
@@ -130,11 +130,11 @@ stdenv.mkDerivation (finalAttrs: {
     ./patches/dont-link-opencv-world-orca.patch
     # The changeset from https://github.com/SoftFever/OrcaSlicer/pull/7650, can be removed when that PR gets merged
     # Allows disabling the update nag screen
-    (fetchpatch {
-      name = "pr-7650-configurable-update-check.patch";
-      url = "https://github.com/SoftFever/OrcaSlicer/commit/d10a06ae11089cd1f63705e87f558e9392f7a167.patch";
-      hash = "sha256-t4own5AwPsLYBsGA15id5IH1ngM0NSuWdFsrxMRXmTk=";
-    })
+    # (fetchpatch {
+    #   name = "pr-7650-configurable-update-check.patch";
+    #   url = "https://github.com/SoftFever/OrcaSlicer/commit/d10a06ae11089cd1f63705e87f558e9392f7a167.patch";
+    #   hash = "sha256-t4own5AwPsLYBsGA15id5IH1ngM0NSuWdFsrxMRXmTk=";
+    # })
   ];
 
   doCheck = true;
